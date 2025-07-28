3d5583da33e25d5ee02552b4dbed3c49222515ed
blastonyz <blastonyzamora@gmail.com>
2025-07-28 01:20:42 -0300

    cards colors

--- app/components/cards/Card.tsx
+++ app/components/cards/Card.tsx
@@ -2,26 +2,29 @@ import type { MenuCategory } from "@/app/types"
 import style from './card.module.css'
 
 
-const Card = ({link}:{link:MenuCategory }) => {
+const Card = ({ link }: { link: MenuCategory }) => {
   return (
-    <section className={style.mainCard}>
-       <h3>{link.item}</h3>
+    <section className={style.cardBorder}>
 
-        {   link.products.map((product,index)=>(
-            <div className={style.product} key={index}>
-               <div className={style.dish}> <p>{product.name}</p>
-               <p>{product.price}</p>
-               </div>
+      <div className={style.mainCard}>
+        <div className={style.categoryTitleContainer}> <h3 className={style.categoryTitle}>{link.item}</h3></div>
+
+        {link.products.map((product, index) => (
+          <div className={style.product} key={index}>
+            <div className={style.dish}> <p className={style.productTitle}>{product.name}</p>
+              <p className={style.productPrice}>{product.price}</p>
             </div>
+          </div>
         ))
 
         }
 
 
 
+      </div>
 
     </section>
   )
 }
 
-export  default Card
+export default Card
